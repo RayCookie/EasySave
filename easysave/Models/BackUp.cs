c059ac5d8404186cd7a8d4f448d79e61c7ce16ba
Mohamed DIAG <Mohamed.DIAG.dz@viacesi.fr>
2021-11-21 16:25:16 +0100

    push (modification BACKUP

--- easysave/Models/BackUp.cs
+++ easysave/Models/BackUp.cs
@@ -8,5 +8,19 @@ namespace easysave.Models
 {
     class BackUp
     {
-    }
+        // Declaration of properties that are used for saving backup information for the backup job file
+        public string SourceDir { get; set; }
+        public string TargetDir { get; set; }
+        public string SaveName { get; set; }
+        public int Type { get; set; }
+        public string MirrorDir { get; set; }
+
+        public BackUp(string saveName, string sourceDir, string targetDir, int type, string mirrorDir)
+        {
+            SaveName = saveName;
+            SourceDir = sourceDir;
+            TargetDir = targetDir;
+            Type = type;
+            MirrorDir = mirrorDir;
+        }
 }
