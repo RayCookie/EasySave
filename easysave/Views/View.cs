19e4b1f200dd63038570faee112f2fa600e61565
Othmane HADDOUCHE <othmane.haddouche.dz@viacesi.fr>
2021-11-24 00:10:42 +0100

    UML V1

--- easysave/Views/View.cs
+++ easysave/Views/View.cs
@@ -7,69 +7,137 @@ namespace easysave.view
 {
     class View
     {
-
-        public void Languuage()
+        public void Welcome()
         {
-            Console.WriteLine("***   Choose a language      ***");
-            Console.WriteLine("***   1.Francais             ***");
-            Console.WriteLine("***   2.Anglais              ***");
+            Console.WriteLine(" _________________________________________________________________________ ");
+            Console.WriteLine("| ____|     /   | /  ___/ | |  / /      /  ___/     /   | | |   / / | ____|");
+            Console.WriteLine("| |__      / /| | | |___   | |/ /       | |___     / /| | | |  / /  | |__  ");
+            Console.WriteLine("|  __|    / / | | |___  |   |  /         ____     / / | | | | / /   |  __| ");
+            Console.WriteLine("| |___   / /  | |  ___| |   / /          ___| |  / /  | | | |/ /    | |___ ");
+            Console.WriteLine("|_____| /_/   |_| /_____/  /_/          /_____/ /_/   |_| |___/     |_____|");
+        }
+
+        public void Language()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++______________++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++++____|              |_____++++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++|                         |+++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++ |    Choose a language    |+++++++++++++++++++++++++++");
+            Console.WriteLine(" _________________________|                         |__________________________ ");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                          1 +-----Francais----+----+                          |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                          2 +-----English-----+----+                          |");
+            Console.WriteLine("|_________________________                           _________________________ |");
+            Console.WriteLine("+++++++++++++++++++++++++ |                         |+++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++ |                         |+++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++|_________________________|+++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         }
 
         public void DisplayMenu()
         {
-            Console.WriteLine("***                    Menu                      ***");
-            Console.WriteLine("****************************************************");
-            Console.WriteLine("***            0. Exit                           ***");
-            Console.WriteLine("***            1. Execute a backup job           ***");
-            Console.WriteLine("***            2. Create a backup job            ***");
-            Console.WriteLine("****************************************************");
-            Console.Write("Please enter a number : ");
+            Console.WriteLine("++++++++++++++++++++++++++++++++______________++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++++____|              |_____++++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++|                         |+++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++ |           Menu          |+++++++++++++++++++++++++++");
+            Console.WriteLine(" _________________________|                         | _________________________");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                     0 +-------------Exit--------+----------+                 |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                     1 +------Execute a backup job-----+----+                 |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|                     2 +------Create a backup job-----+-----+                    |");
+            Console.WriteLine("|                                                                              |");
+            Console.WriteLine("|_________________________                               _____________________ |");
+            Console.WriteLine("+++++++++++++++++++++++++ |                             |++++++++++++++++++++++|");
+            Console.WriteLine("+++++++++++++++++++++++++ |                             |++++++++++++++++++++++|");
+            Console.WriteLine("++++++++++++++++++++++++++|    Please enter a number    |++++++++++++++++++++++|");
+            Console.WriteLine("++++++++++++++++++++++++++|_____________________________|");
         }
 
-        public void DisplayMenu2()
+        public void DisplaySubMenu()
         {
-
-            Console.WriteLine("***                 Backup Jobs                  ***");
-            Console.WriteLine("****************************************************");
-            Console.WriteLine("***  0. Exit                                     ***");
-            Console.WriteLine("***  1. Complete Save                            ***");
-            Console.WriteLine("***  2. Differential Save                        ***");
-            Console.WriteLine("****************************************************");
-            Console.Write("Please enter a number : ");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++______________+++++++++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++++_____|              |_____+++++++++++++++++++++++++++");
+            Console.WriteLine("++++++++++++++++++++++++++|                          |++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++++++++++++++++++++ |      Backup Jobs         |++++++++++++++++++++++++++");
+            Console.WriteLine(" _________________________|                          | _________________________");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|                     0 +------------Exit--------+--------+                     |");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|                     1 +--------Complete Save--------+----+                    |");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|                     2 +------Differential Save-----+----+                     |");
+            Console.WriteLine("|                                                                               |");
+            Console.WriteLine("|_________________________                               ______________________ |");
+            Console.WriteLine("+++++++++++++++++++++++++ |                             |++++++++++++++++++++++|");
+            Console.WriteLine("+++++++++++++++++++++++++ |                             |++++++++++++++++++++++|");
+            Console.WriteLine("++++++++++++++++++++++++++|    Please enter a number    |++++++++++++++++++++++|");
+            Console.WriteLine("++++++++++++++++++++++++++|_____________________________|");
         }
-
 
         public void ShowSourcePath()
         {
-            Console.WriteLine("Please enter the path of the folder you want to back up.");
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("|                                                        |");
+            Console.WriteLine("|Please enter the path of the folder you want to back up.|");
+            Console.WriteLine("|                                                        |");
+            Console.WriteLine("+ ---------+---------------------------------------------+");
         }
 
         public void ShowTargetPath()
         {
-            Console.WriteLine("Please enter the destination path for the backup.");
+            Console.WriteLine("+-------------------------------------------------+");
+            Console.WriteLine("|                                                 |");
+            Console.WriteLine("|Please enter the destination path for the backup.|");
+            Console.WriteLine("|                                                 |");
+            Console.WriteLine("+ ---------+--------------------------------------+");
         }
 
-        public void ShowNewPath()
+        public void ShowMirrorPath()
         {
-            Console.WriteLine("Please enter the path of the folder mirror backup. [ DRAG AND DROP YOUR FOLDER ] :");
+            Console.WriteLine("+--------------------------------------------------+");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("|Please enter the path of the folder mirror backup.|");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("+ ---------+---------------------------------------+");
         }
 
         public void ShowFile()
         {
-            Console.Write("Please enter the name of your backup : ");
+            Console.WriteLine("+-------------------------------------+");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|Please enter the name of your backup.|");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("+ ---------+--------------------------+");
         }
+
 
         public void ShowNameFile()
         {
             Console.Clear();
-            Console.WriteLine("Here are the names of your backups : ");
+            Console.WriteLine("+-------------------------------------+");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|Here are the names of your backups.  |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("+ ---------+--------------------------+");
         }
+
         public void ErrorMenu(string result)
         {
-            Console.WriteLine(result);
+            Console.WriteLine("+-----------------+-------------------+");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|"             +result+ "             |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("+ ----------------+-------------------+");
         }
-
-
-
     }
 }
